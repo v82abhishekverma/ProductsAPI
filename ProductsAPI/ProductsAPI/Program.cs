7b22b56c6f8ae5806af4b20141881dcbb13150fa
v82abhishekverma <61831053+v82abhishekverma@users.noreply.github.com>
2022-08-04 15:58:27 +0100

    first

--- ProductsAPI/ProductsAPI/Program.cs
+++ ProductsAPI/ProductsAPI/Program.cs
@@ -15,6 +15,7 @@ namespace ProductsAPI
         {
             CreateHostBuilder(args).Build().Run();
         }
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
